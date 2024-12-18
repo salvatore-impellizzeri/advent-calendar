e9e923be6e5e8fecdf7cccdababf33a8aacb7c7a
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-18 01:02:44 +0100

    Feat/Main fix + modal created

--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,19 +1,23 @@
 <script>
 import { store } from '../../store';
+import Modal from './components/Modal.vue';
 
 export default {
   data() {
     return {
         store,
+        modal: false,
     }
   },
 
   components: {
-
+    Modal,
   },
 
   methods:{
-
+    openModal(){
+      modal = true
+    }
   }
 }
 </script>
@@ -21,16 +25,23 @@ export default {
 <template>
     <div class="grid grid-cols-8 grid-rows-4 gap-6 h-auto py-5 text-center text-white font-semibold">
         <div 
-        class="bg-[#2E6754] rounded-lg flex flex-col items-center justify-center aspect-square" 
-        v-for="(day, index) in store.source" 
-        :key="index" 
-        :class="[index === 24 ? 'bg-[#D4D6F5] col-span-8 text-[#2E6754] aspect-auto' : '']"
+          class="bg-[#2E6754] rounded-lg flex flex-col items-center justify-center" 
+          v-for="(day, index) in store.source" 
+          :key="index" 
+          @click="openModal"
+          :class="[index === 24 ? 'bg-[#D4D6F5] col-span-8 text-[#2E6754] aspect-auto' : 'aspect-square']"
         >
             <img :src="day.icon" alt=""
             class="scale-100 h-8"
             >
             <span class="text-4xl">{{ index + 1 }}</span>
         </div> 
+    </div>
+    <div>
+      <Modal v-for="(modal, index) in store.source"
+        :gif="modal.url"
+        :text="modal.text"
+      />
     </div>
 </template>
 
---
+++ src/components/Main/components/Modal.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  data() {
+    return {
+      close: false,
+    }
+  },
+
+  props: {
+    gif: String,
+    text: String
+  },
+  methods: {
+    closeModal() {
+      this.close = !this.close;
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="!closeModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div 
+      class="flex flex-col gap-7 justify-center items-center rounded-2xl bg-white py-7 px-5"
+      style="width: 400px;"
+    >
+      <div class="w-full">
+        <img v-if="gif"
+          :src="gif" 
+          alt="Gif"
+          class="w-full h-auto object-contain"
+        >
+        <div v-if="text">
+          <span>{{ text }}</span>
+        </div>
+      </div>
+      <button 
+        @click="closeModal"
+        class="bg-red-600 py-2 px-7 rounded-full text-white text-lg font-semibold"
+      >
+        Chiudi
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
